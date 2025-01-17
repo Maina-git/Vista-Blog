681c6b495265932a096bb434290748eda181ab79
maina-git <francismm2023@gmail.com>
2025-01-17 17:13:24 +0300

    new changes

---
+++ vite/src/components/feed/PostFeed.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostFeed = () => {
+  return (
+    <div className="w-3/4 h-screen bg-white rounded-lg m-2">
+      
+    </div>
+  )
+}
+
+export default PostFeed
--- vite/src/pages/CreatePost.tsx
+++ vite/src/pages/CreatePost.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import UserInfo from '../components/profile/UserInfo';
+import PostFeed from '../components/feed/PostFeed';
+import ProfileFeed from '../components/feed/ProfileFeed';
 
 const CreatePost = () => {
   return (
-    <div className="bg-gray-100 w-full h-screen">
-      
+    <div className="bg-gray-100 w-full h-screen flex">
+      <PostFeed/>
+      <ProfileFeed/>
     </div>
   )
 }
